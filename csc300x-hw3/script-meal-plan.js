c2b92810c52c338900b101b14ab1bfdea9909cd2
Natchapone Janjon <n_janjon@uncg.edu>
2024-02-14 14:30:00 -0500

    patch 0.2.0

--- csc300x-hw3/script-meal-plan.js
+++ csc300x-hw3/script-meal-plan.js
@@ -4,22 +4,27 @@ function addMeal(button) {
     const cost = button.parentElement.getAttribute("data-price");
     const selectedMeal = document.getElementById("selectedMeal");
 
-    order.innerHTML = `${item} <button onclick="removeOrder(this, ${cost})">Remove</button> 
-    <span class="cost" hidden>${cost}</span>`;
+    order.innerHTML = `${item} <span class="cost" hidden>${cost}</span>`;
+
+    var removeBtn = document.createElement("button");
+    removeBtn.textContent = 'Remove';
+
+    removeBtn.onclick = function () {
+        order.remove();
+        calculateCost();
+    }
 
     selectedMeal.appendChild(order);
-    Calculate();
+    order.appendChild(removeBtn);
+
+    calculateCost();
+
 }
 
-function removeOrder(button) {
-    const dishElement = button.parentElement;
-    dishElement.remove();
-    Calculate();
-}
-
-function Calculate() {
+function calculateCost() {
     const prices = document.querySelectorAll("#selectedMeal .cost");
     let total = 0;
     prices.forEach((amount) => { total += parseFloat(amount.textContent); });
     document.getElementById("totalCost").textContent = total.toFixed(2);
+
 }
